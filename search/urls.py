2936151dcdf9dbe3ecff97b780130dc404b17c62
vinayakentc <66103397+vinayakentc@users.noreply.github.com>
2020-07-31 22:14:31 +0530

    Add files via upload

---
+++ search/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import (
+        SearchProductView
+        )
+app_name='search'
+
+urlpatterns = [
+    path('', SearchProductView.as_view(), name='query'),
+]
